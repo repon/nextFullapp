d40bf3e7af019da8683109d7482b444708f6a9e9
repon <repon27@yahoo.co.jp>
2022-08-17 17:39:03 +0900

    上巻（サーバサイド）終了

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -1,5 +1,8 @@
+import jwt from 'jsonwebtoken';
 import connectDB from '../../../utils/database';
 import { UserModel } from '../../../utils/schemaModels';
+
+const secret_key = process.env.SECRET_KEY
 
 const loginUser = async (req, res) => {
   try {
@@ -7,7 +10,14 @@ const loginUser = async (req, res) => {
     const saveUserData = await UserModel.findOne({ email: req.body.email })
     if (saveUserData) {
       if (req.body.password === saveUserData.password) {
-        return res.status(200).send({ message: "ログイン成功" })
+        const payload = {
+          email: req.body.email,
+        }
+
+        const token = jwt.sign(payload, secret_key, { expiresIn: "23h" })
+        console.log(token)
+
+        return res.status(200).send({ message: "ログイン成功", token: token })
       } else {
         return res.status(400).send({ message: "ログイン失敗：パスワードが違います" })
       }
